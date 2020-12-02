f3bec9f497d3b62ddc5b0adb4bcb15c1eb1feef3
Totti_535 <55733062@qq.com>
2020-12-02 14:15:18 +0800

    test batch size

--- tail-based-sampling/src/main/java/com/alibaba/tailbase/clientprocess/ClientProcessData.java
+++ tail-based-sampling/src/main/java/com/alibaba/tailbase/clientprocess/ClientProcessData.java
@@ -31,7 +31,7 @@ public class ClientProcessData implements Runnable {
     // an list of trace map,like ring buffe.  key is traceId, value is spans ,  r
     public static Map<Integer, List<Map<String, List<String>>>> BATCH_TRACE_LIST = new ConcurrentHashMap<>();
     // make 50 bucket to cache traceData
-    public static int BATCH_COUNT = 15;
+    public static int BATCH_COUNT = 8;
 
     private static int OVERLAP_BUFFER = 1000;
 
