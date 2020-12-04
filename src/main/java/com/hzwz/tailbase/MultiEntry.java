3e1daf53a38ff51f5e320877bdfd4f61dfe36e0a
Totti_535 <55733062@qq.com>
2020-12-04 13:49:47 +0800

    update project structure

--- src/main/java/com/hzwz/tailbase/MultiEntry.java
+++ src/main/java/com/hzwz/tailbase/MultiEntry.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
-@ComponentScan(basePackages = "com.alibaba.tailbase")
+@ComponentScan(basePackages = "com.hzwz.tailbase")
 public class MultiEntry {
 
 
